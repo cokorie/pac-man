6a4c3c37120105b13480a5d9250a692f2bee30e4
cokorie <chijioke.okorie@gmail.com>
2023-08-14 22:03:39 -0500

    update wallSpaceWidth to divide by 1.5

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let createRect = (x, y, width, height, color) => {
 let fps = 30;
 let oneBlockSize = 20;
 let wallColor = "#342DCA";
-let wallSpaceWidth = oneBlockSize / 1.3;
+let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
 

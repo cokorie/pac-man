a59bba6ff40deb20ccbe3d69556e09467210bff9
cokorie <chijioke.okorie@gmail.com>
2023-08-23 12:57:03 -0500

    add nested for loop for eat

--- pacman.js
+++ pacman.js
@@ -25,7 +25,9 @@ class Pacman {
 
     eat() {
         for (let i = 0; i < map.length; i++) {
-            
+            for (let j = 0; j < map.length; j++) {
+                
+            }
         }
     }
 

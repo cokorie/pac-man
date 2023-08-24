3a24f3361f927951648c6c998de554bc2323e459
cokorie <chijioke.okorie@gmail.com>
2023-08-24 06:04:40 -0500

    add fourth val for randomTargetsForGhosts

--- game.js
+++ game.js
@@ -28,6 +28,7 @@ let randomTargetsForGhosts = [
     {x: 1 * oneBlockSize, y: 1 * oneBlockSize},
     {x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize},
     {x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize},
+    {x: (map[0].length - 2) * oneBlockSize, y: (map.length - 2) * oneBlockSize},
 ];
 
 let ghostLocations = [

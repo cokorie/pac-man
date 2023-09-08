fc991cdd60d0f74eac5592c1915d3ef73d475b45
cokorie <chijioke.okorie@gmail.com>
2023-09-08 13:12:39 -0500

    update Game Over text x placement to 150

--- game.js
+++ game.js
@@ -100,7 +100,7 @@ let gameOver = () => {
 let drawGameOver = () => {
     canvasContext.font = "20px Emulogic";
     canvasContext.fillStyle = "white";
-    canvasContext.fillText("Game Over!", 200, 200);
+    canvasContext.fillText("Game Over!", 150, 200);
 };
 
 let drawLives = () => {

6b7b9246a6bfd1cbabbc01f5c177b00086fca419
cokorie <chijioke.okorie@gmail.com>
2023-09-08 13:04:10 -0500

    add oneBlockSize multiple 2

--- game.js
+++ game.js
@@ -101,7 +101,7 @@ let drawLives = () => {
     canvasContext.fillStyle = "white";
     canvasContext.fillText("Lives: ", 220, oneBlockSize * (map.length + 1) + 10);
     for (let i = 0; i < lives; i++) {
-        canvasContext.drawImage(pacmanFrames);
+        canvasContext.drawImage(pacmanFrames, 2 * oneBlockSize,);
     }
 }
 

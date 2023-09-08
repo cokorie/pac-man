1d85cc64d5939d78fd36ee4b87e4cba83364e4c6
cokorie <chijioke.okorie@gmail.com>
2023-09-08 12:59:18 -0500

    add 10 to text sizing

--- game.js
+++ game.js
@@ -99,7 +99,7 @@ let gameOver = () => {
 let drawLives = () => {
     canvasContext.font = "20px Emulogic";
     canvasContext.fillStyle = "white";
-    canvasContext.fillText("Lives: ", 200, oneBlockSize * (map.length + 1));
+    canvasContext.fillText("Lives: ", 200, oneBlockSize * (map.length + 1) + 10);
 }
 
 let drawFoods = () => {

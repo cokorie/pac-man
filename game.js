15491e99515d48d1b713ddfe6160f4252bd2b50f
cokorie <chijioke.okorie@gmail.com>
2023-12-01 04:21:25 -0600

    move lives variable

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
+let lives = 3;
 
 let fps = 30;
 let oneBlockSize = 20;
@@ -23,7 +24,6 @@ let foodColor = "#FEB897";
 let score = 0;
 let ghosts = [];
 let ghostCount = 4;
-let lives = 3;
 let foodCount = 0;
 
 let ghostLocations = [

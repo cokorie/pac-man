ef6e3d83c9711dfe62a744a0b28d099884865b08
cokorie <chijioke.okorie@gmail.com>
2023-12-04 03:45:23 -0600

    add 6th and 7th oneBlockSize

--- game.js
+++ game.js
@@ -169,7 +169,8 @@ let drawRemainingLives = () => {
             oneBlockSize,
             350 + i * oneBlockSize,
             oneBlockSize * map.length + 2,
-            
+            oneBlockSize,
+            oneBlockSize
         );
     }
 };

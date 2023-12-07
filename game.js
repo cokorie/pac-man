880e79fce2abbfdad4cf84fc45f70490fb0b7b59
cokorie <chijioke.okorie@gmail.com>
2023-12-07 08:08:38 -0600

    update ghost game for ghostFrames

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 const pacmanFrames = document.getElementById("animations");
-const ghostFrames = document.getElementById("ghosts");
+const ghostFrames = document.getElementById("ghost");
 
 let createRect = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;

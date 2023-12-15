79d677118b7bf72b815f60835307157a5ad9d5d5
cokorie <chijioke.okorie@gmail.com>
2023-12-15 08:47:03 -0600

    update setInterval to 100 seconds

--- ghost.js
+++ ghost.js
@@ -15,7 +15,7 @@ class Ghost {
         this.target = randomTargetsForGhosts[this.randomTargetIndex];
         setInterval(() => {
             this.changeRandomDirection()
-        }, 10000);
+        }, 100000);
     }
 
     isInRange() {

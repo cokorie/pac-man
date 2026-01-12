84333c38db496d85c3f3034d30a1752300edf2d4
cokorie <chijioke.okorie@gmail.com>
2026-01-12 08:47:23 -0600

    add tileMap

--- pacman.js
+++ pacman.js
@@ -17,6 +17,32 @@ let pacmanLeftImage;
 let pacmanRightImage;
 let wallImage;
 
+//X = Wall, O = Skip Path, p = Pac-Man, ' ' = food pellets
+//b = Blue Ghost, o = Orange Ghost, P = Pink Ghost, r = Red Ghost
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX" 
+];
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;

5c5c3a9305fc1ecff10f00a0cad47c25aa128a39
cokorie <chijioke.okorie@gmail.com>
2026-01-12 08:38:28 -0600

    add pac right location

--- pacman.js
+++ pacman.js
@@ -43,4 +43,5 @@ function loadImage() {
     pacmanDownImage.src = "./assets/pacmanDown.png";
     pacmanLeftImage = new Image();
     pacmanLeftImage.src = "./assets/pacmanLeft.png";
+    pacmanRightImage = new Image();
 }

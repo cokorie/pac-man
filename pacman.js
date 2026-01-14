5360fea697d675bfccfaa8ba63b92d77d374eb42
cokorie <chijioke.okorie@gmail.com>
2026-01-14 09:38:57 -0600

    clear collision functionality

--- pacman.js
+++ pacman.js
@@ -170,7 +170,7 @@ function movePacman(e) {
 }
 
 function collision(a, b) {
-
+    return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 }
 
 class Block {

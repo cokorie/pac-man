4e043bcbfaf04ee14f6048e136c75310118ab047
cokorie <chijioke.okorie@gmail.com>
2026-01-14 13:36:26 -0600

    update resetPosition to resetPositions

--- pacman.js
+++ pacman.js
@@ -245,7 +245,7 @@ function collision(a, b) {
     return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 }
 
-function resetPosition() {
+function resetPositions() {
     pacman.reset();
     pacman.velocityX = 0;
     pacman.velocityY = 0;

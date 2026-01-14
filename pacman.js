4915771499c790c256fc7f0daca19f5819853f0e
cokorie <chijioke.okorie@gmail.com>
2026-01-14 09:06:00 -0600

    add 2nd else if left condition

--- pacman.js
+++ pacman.js
@@ -176,8 +176,8 @@ class Block {
             this.velocityX = 0;
             this.velocityY = tileSize / 4;
         }
-        else if () {
-            
+        else if (this.direction == 'L') {
+
         }
     }
 }

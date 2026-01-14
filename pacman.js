1b9eaf76ba8a8d797e3986d7f47da3fb7132940c
cokorie <chijioke.okorie@gmail.com>
2026-01-14 09:03:44 -0600

    add else if conditional

--- pacman.js
+++ pacman.js
@@ -172,8 +172,8 @@ class Block {
             this.velocityX = 0;
             this.velocityY = -tileSize / 4;
         }
-        else if () {
-            
+        else if (this.direction == 'D') {
+
         }
     }
 }
